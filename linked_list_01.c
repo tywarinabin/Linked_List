c672d20d9b1023a625d7afe29e7bdb2bd779979e
Nabin Tiwari <96787565+tywarinabin@users.noreply.github.com>
2022-08-10 09:41:14 +0530

    Add files via upload
    Single Linked List Traversak

---
+++ linked_list_01.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node *link;
+};
+void traverse(struct node *ptr){
+    static int count;
+   while(ptr!= NULL){
+     printf("%d  ", ptr->data);
+    ptr = ptr->link;
+   count++;
+   }
+   printf("No. of NODES = %d ", count);
+}
+int main(){
+  struct node *head;
+   struct node *second;
+   struct node *last;
+   head = (struct node *) malloc(sizeof(struct node));
+   second = (struct node *) malloc(sizeof(struct node));
+   last = (struct node *) malloc(sizeof(struct node));
+   head->data = 67;
+   head->link = second;
+   second->data = 77;
+   second->link = last;
+   last->data = 87;
+   last->link = NULL;
+//    printf("%u   ", second->link);
+//     printf("%u", &last->data);
+
+/*---> PASSING HEAD TO THE POINTER */ 
+traverse(head);  
+return 0;
+}
